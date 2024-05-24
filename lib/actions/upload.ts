50fa3898deedf3d53fda48640056f0fcfb0d5e4c
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-05-24 14:12:06 +0330

    checkout session

--- lib/actions/upload.ts
+++ lib/actions/upload.ts
@@ -11,10 +11,13 @@ import {
   CaseFinish,
   CaseMaterial,
   Configuration,
+  Order,
   PhoneModel,
   Uploader,
 } from '@prisma/client'
 import sharp from 'sharp'
+import { currentUser } from '../auth'
+import { BASE_PRICE, PRODUCT_PRICES } from '@/config/products'
 
 interface CreateUploadFormState {
   // success?: string
@@ -202,4 +205,80 @@ export async function updateUpload(
   revalidatePath(path)
   // /configure/preview?id=${configId}
   redirect(`/configure/preview?id=${configure.id}`)
+}
+
+/////////////////////////
+
+export const createCheckoutSession = async ({
+  configId,
+}: {
+  configId: string
+}) => {
+  const configuration = await prisma.configuration.findUnique({
+    where: { id: configId },
+  })
+
+  if (!configuration) {
+    throw new Error('No such configuration found')
+  }
+
+  const user = await currentUser()
+
+  if (!user?.id) {
+    throw new Error('You need to be logged in')
+  }
+
+  const { finish, material } = configuration
+
+  let price = BASE_PRICE
+  if (finish === 'textured') price += PRODUCT_PRICES.finish.textured
+  if (material === 'polycarbonate')
+    price += PRODUCT_PRICES.material.polycarbonate
+
+  let order: Order | undefined = undefined
+
+  const existingOrder = await prisma.order.findFirst({
+    where: {
+      userId: user.id,
+      configurationId: configuration.id,
+    },
+  })
+
+  // console.log(user.id, configuration.id)
+
+  if (existingOrder) {
+    order = existingOrder
+  } else {
+    order = await prisma.order.create({
+      data: {
+        amount: price / 100,
+        userId: user.id,
+        configurationId: configuration.id,
+      },
+    })
+  }
+
+  // const product = await stripe.products.create({
+  //   name: 'Custom iPhone Case',
+  //   images: [configuration.imageUrl],
+  //   default_price_data: {
+  //     currency: 'USD',
+  //     unit_amount: price,
+  //   },
+  // })
+
+  // const stripeSession = await stripe.checkout.sessions.create({
+  //   success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+  //   cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
+  //   payment_method_types: ['card', 'paypal'],
+  //   mode: 'payment',
+  //   shipping_address_collection: { allowed_countries: ['DE', 'US'] },
+  //   metadata: {
+  //     userId: user.id,
+  //     orderId: order.id,
+  //   },
+  //   line_items: [{ price: product.default_price as string, quantity: 1 }],
+  // })
+
+  return { orderId: order.id }
 }
